4b219ce52ce636c7f07f4ca50a3ae776fc4e8c9c
HelioChun <heliochun@pulseframe.com>
2015-05-06 13:35:12 +0100

    Q2A Read More Version 1.0

--- qa-read-more-layer.php
+++ qa-read-more-layer.php
@@ -15,11 +15,10 @@ class qa_html_theme_layer extends qa_html_theme_base {
 		global $qa_request;
 		qa_html_theme_base::head_css();
 		$this->output('<style type="text/css">
-		/* Style your Read More Button here */
-
+/* Style your Read More Button here */
+/*
 .readmore-button {
 	text-transform: uppercase;
-	/*
 	display: block;
 	width: 100%;
 	text-align: center;
@@ -32,17 +31,15 @@ class qa_html_theme_layer extends qa_html_theme_base {
 	padding: 2px 9px 2px 8px;
 	background-color: #fcfcfc;
 	background-image: linear-gradient(rgba(255,255,255,0),rgba(235,235,235,.3)60%,rgba(225,225,225,.6));
-	*/
 }
 
-/*
 .readmore-button:hover {
 	background-color: #f9f9f9;
 	border-color: #e5e5e5 #ccc #ccc;
 	box-shadow: 0 1px 3px rgba(0,0,0,.15),inset 0 1px 0 rgba(255,255,255,1),inset -1px 0 0 rgba(248,248,248,1),inset 0 -1px 0 rgba(248,248,248,.5),inset 1px 0 0 rgba(248,248,248,1);
 	text-shadow: -1px -1px 0 #fcfcfc,1px -1px 0 #fcfcfc,-1px 1px 0 #fcfcfc,1px 1px 0 #fff;
 }
-		*/
+*/
 		</style>');
 	}
 
